a496f340696eb51cf458c94e59b1ca4a0390171a
Radostina Emilova <118113591+Radostinaaaa@users.noreply.github.com>
2023-01-17 16:41:48 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+  let elementsPrice=document.querySelectorAll('.price'&&'.hot');
+  for(let i=0; i<elementsPrice.length; i++) {
+      elementsPrice[0].innerHTML+=' &#128293;';
+  }
 });
